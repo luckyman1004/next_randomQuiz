42afb692b9572fa4f530cd2bacc0f84e6184f15a
luckyman1004 <luckysuperdev@gmail.com>
2021-01-25 20:54:14 -0300

    fix(default-theme): Fix type error 🌝
    * Create a declaration file
    * Create a theme file
    * Replace configuration of theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "@/styles/theme";
 
 import GlobalStyle from "@/styles/global";
 
-import db from "../../db.json";
+// import db from "../../db.json";
 
-const theme = db.theme;
+// const theme = db.theme;
+
+export type ThemeType = typeof defaultTheme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={defaultTheme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
