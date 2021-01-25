91f251f5e7fa1a1ab2655e3c0ff2d2928bf58f7e
luckyman1004 <luckysuperdev@gmail.com>
2021-01-25 19:52:59 -0300

    feat(structure): Create the basic page architecture 🐙
    * Add src folder
    * Add src/components folder
    * Add src/pages folder
    * Add src/styles
    * Add Footer component
    * Add GitHubCorner component
    * Configure root imports

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import { FooterWrapper } from "@/styles/components/footer";
+
+export default function Footer(props) {
+  return (
+    <FooterWrapper {...props}>
+      <a href="https://www.alura.com.br/">
+        <img
+          src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg"
+          alt="Logo Alura"
+        />
+      </a>
+      <p>
+        Criado durante a{" "}
+        <a href="https://www.alura.com.br/">
+          <span>Imersão React da Alura</span>
+        </a>
+      </p>
+    </FooterWrapper>
+  );
+}
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+interface BackgroundProps {
+  backgroundImage: string;
+}
+
+export const BackgroundImage = styled.main<BackgroundProps>`
+  background-image: url(${({ backgroundImage }) => backgroundImage});
+  background-position: center;
+  background-size: cover;
+  flex: 1;
+`;
+
+export const QuizContainer = styled.section`
+  margin: auto 10%;
+  max-width: 350px;
+  padding-top: 45px;
+  width: 100%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export const Widget = styled.aside`
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  margin-bottom: 24px;
+  margin-top: 24px;
+  overflow: hidden;
+
+  h1,
+  h2,
+  h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+
+export const WidgetHeader = styled.header`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.primary};
+  display: flex;
+  justify-content: flex-start;
+  padding: 18px 32px;
+
+  * {
+    margin: 0;
+  }
+`;
+
+export const WidgetContent = styled.header`
+  padding: 24px 32px 32px 32px;
+
+  & > *:first-child {
+    margin-top: 0;
+  }
+
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
